6b986f8a0ed27245196c564891f0592d7f78ffa6
lusteiger <leonardoxy7@gmail.com>
2020-09-06 14:52:44 +0800

    exercise-01-homework

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable no-unused-vars */
 export default function formatNumber(number, option) {
   // This function will format the number to a fixed number string. The decimal part should 2.
   // That means the number 2 will be formatted as '2.00'. The `option` is an object which contains
@@ -8,5 +11,12 @@ export default function formatNumber(number, option) {
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
 
+  var format = number.toFixed(2);
+  if (option != null) {
+    if (option.currency == true) {
+      format = "$ " + format;
+    }
+  }
+  return format;
   throw new Error('Please delete this line and implement the function');
 }
