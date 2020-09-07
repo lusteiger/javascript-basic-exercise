f58ade564c28e47d9128ad2b31a1bda95b0ee7b7
lusteiger <leonardoxy7@gmail.com>
2020-09-07 22:40:56 +0800

    exercise-04-homework

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,42 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
+
+  if (array === undefined) {
+    throw 'Creating list from undefined array';
+  }
+
+  if (array === null) {
+    throw 'Creating list from undefined array';
+  }
+
+  if (array.length === 0) {
+    throw 'Creating list from empty array';
+  }
+
+  const list = new Array();
+  var m = 0;
+  for (let i = array.length - 1; i >= 0; i--) {
+
+    let arr;
+    if (i === array.length - 1) {
+      arr = {
+        value: array[i],
+        next: null,
+      }
+      list.push(arr);
+    }
+    else {
+      arr = {
+        value: array[i],
+        next: list[m],
+      }
+      list.push(arr);
+      m++;
+    }
+  }
+  list.reverse();
+  return list[0];
+
   throw new Error('Please delete this line and implement the function');
 }
