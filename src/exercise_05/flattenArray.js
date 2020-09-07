b75622a6d0bd5790b967315cb1f1d1eb2e62499e
lusteiger <leonardoxy7@gmail.com>
2020-09-07 11:03:09 +0800

    exerise-05-homework

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -1,3 +1,8 @@
+function flatDeep(arr, d) {
+  return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+    : arr.slice();
+};
+
 export default function flattenArray(array) {
   // This function flattens a nested array into a sequence.
   //
@@ -5,6 +10,15 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
+
+  if (array === null || array === undefined) {
+    throw 'Flatten undefined or null array';
+  }
+  else {
+    var flatArray = flatDeep(array, 1);
+    return flatArray;
+  }
+
 
   throw new Error('Please delete this line and implement the function');
 }
