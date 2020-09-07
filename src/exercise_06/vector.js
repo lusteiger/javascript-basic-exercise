0d69454e0186abd424c47dafdfa6ac443edf80eb
lusteiger <leonardoxy7@gmail.com>
2020-09-07 18:11:57 +0800

    exercise-06-homework

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -13,4 +13,35 @@ export default class Vector {
   // class.
 
   // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+
+
+  constructor(a, b) {
+    let object = {
+      x: 0,
+      y: 0,
+      distance: function () {
+        return Math.hypot(a, b);
+      }
+    };
+    Object.defineProperty(object, 'x', {
+      value: a,
+      writable: false,
+    });
+
+    Object.defineProperty(object, 'y', {
+      value: b,
+      writable: false,
+    });
+    return object;
+
+  }
+
+  static plus(a, b) {
+    return new Vector(a.x + b.x, a.y + b.y);
+  }
+
+  static minus(a, b) {
+    return new Vector(a.x - b.x, a.y - b.y);
+  }
+
 }
